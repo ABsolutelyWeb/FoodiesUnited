caafa8c2ef289df34f95c0653298efdf2c1354c7
Abhishek <awittlegupta@gmail.com>
2016-04-10 23:39:39 +0000

    Comment editing implemented

--- routes/comments.js
+++ routes/comments.js
@@ -52,6 +52,32 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 
+// EDIT ROUTE for comments which takes us to a form.
+router.get("/:comment_id/edit", function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {restaurant_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+
+// UPDATE ROUTE for comments which takes our form input
+// and updates the comment text.
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/restaurants/" + req.params.id);
+        }
+    });
+});
+
+
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
