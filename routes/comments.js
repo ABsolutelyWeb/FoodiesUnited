4b48e30765c8ddb871702dfd9ef2bde2f0c9971c
Abhishek <awittlegupta@gmail.com>
2016-04-11 00:01:19 +0000

    Implemented destroy route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -78,6 +78,19 @@ router.put("/:comment_id", function(req, res) {
 
 
 
+// DESTROY route for comments.
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/restaurants/" + req.params.id);
+        }
+    });
+});
+
+
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
